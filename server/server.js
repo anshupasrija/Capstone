7abc5d53ff8e345ff7a2ac322f7c8ecdc6705a9a
anshupasrija <anshu.pasrija@gmail.com>
2021-03-30 10:19:16 -0700

    Captone Project Submission

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
+const PORT = 5000;
+const signup=require("./routes/signup");
+const login =require("./routes/login");
+const edit =require("./routes/edit");
+const owner=require("./routes/owner");
+
+app.use(morgan("dev"));
+app.use(cors()); 
+app.use(express.json()); 
+
+app.use((req, res, next) => {
+    console.log("incoming request");
+    next();
+  });
+
+
+app.use('/signup',signup);
+ app.use('/login',login);
+ app.use('/edit',edit);
+ app.use('/user',owner);
+
+
+
+app.listen(PORT, console.log(`listening at: http://localhost:${PORT}`));
