e9262c0a67b00b52ebd3cb62415a94d9e4996d96
Anshu Pasrija <anshu.pasrija@gmail.com>
2021-11-10 18:28:30 -0800

    better

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const signup=require("./routes/signup");
 const login =require("./routes/login");
 const edit =require("./routes/edit");
 const owner=require("./routes/owner");
+const path= require('path');
 
 app.use(morgan("dev"));
 app.use(cors()); 
@@ -24,9 +25,9 @@ app.use('/signup',signup);
  app.use('/user',owner);
 
 if(process.env.NODE_ENV === 'production'){
-app.use(express.static('build'));
-app.get('*',(req,res) =>{
-  req.sendFile(path.resolve(__dirname,'build', 'index.html'));
+app.use(express.static(path.join(__dirname +'build')));
+app.get('/*',(req,res) =>{
+  req.sendFile(path.join(__dirname,'build', 'index.html'));
 })
 }
 
